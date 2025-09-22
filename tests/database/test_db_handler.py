efa308f0ff76fe0315becf80aef9bfb1e4d56c45
ambadran <ambadran717@hotmail.com>
2025-09-22 12:12:00 +0300

    delving deep into finance feature datastructuring

--- tests/database/test_db_handler.py
+++ tests/database/test_db_handler.py
@@ -8,6 +8,5 @@ from efficient_tutor_backend.database.db_handler import DatabaseHandler
 from pprint import pp as pprint
 
 def test_db_get_all_tuition_logs(db_handler: DatabaseHandler):
-    all_tuition_logs = db_handler.get_all_tuition_logs()
+    all_tuition_logs = db_handler.get_all_tuition_logs('dcef54de-bc89-4388-a7a8-dba5d8327447')
     pprint(all_tuition_logs)
-    pprint(json.dumps(all_tuition_logs))
